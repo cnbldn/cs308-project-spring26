eccea06d35848f8c47bb5fc9cbd0c8b2340db2ca
aras <aras.samuk@sabanciuniv.edu>
2026-04-10 13:37:46 +0300

    PRJ-22-add-2-dummy-items-to-the-store-page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Login from './components/Login';
 import Register from './components/Register';
 import ProtectedRoute from './components/ProtectedRoute';
 import Cart from './components/Cart';
+import Shop from './components/Shop';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="/shop" element={<Shop />} />
 
         {/* Protected Routes */}
         <Route element={<ProtectedRoute />}>
